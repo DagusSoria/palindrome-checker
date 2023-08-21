2fef91d1483f698490faef662af29193fe15dcc2
Agustin Soria <85994035+DagusSoria@users.noreply.github.com>
2023-08-21 19:57:55 -0300

    Update file.js

--- file.js
+++ file.js
@@ -1,1 +1,20 @@
+function palindrome(str) {
+  str = str.toLowerCase();
+  str = str.match(/[^_\W]/gi);
 
+  let backwardsStr = [];
+  for(let j = str.length - 1 ; j >= 0; j--){
+    backwardsStr.push(str[j])
+  }
+  backwardsStr = backwardsStr.join("");
+  str = str.join("");
+
+  if(backwardsStr === str){
+    return true
+  } else {
+    return false
+  }
+}
+
+palindrome("A man, a plan, a canal. Panama") // true
+palindrome("My age is 0, 0 si ega ym.") // true
